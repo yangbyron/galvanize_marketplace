ae7e4380b02a2fa3db53df075f8d170122a0f44f
Lguzman9 <guuzman.luis.6@gmail.com>
2022-11-10 17:20:00 -0600

    first changes for login page

---
+++ Frontend/src/Components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login = () => {
+
+  
+
+  return (
+    <div>Login Page
+      <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+            <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Login
