39e05ff7504bdade52c1b77ab207bb109e4e5e53
Lguzman9 <guuzman.luis.6@gmail.com>
2022-11-14 11:06:04 -0600

    login test

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Login = () => {
 
-  
+    const handleSubmit = (e) => {
+      console.log(e.target)
+      // useEffect(
+      //   fetch('localhost:3001/user/api/:id')
+      // )
+    }
 
   return (
     <div>Login Page
-      <form>
+      <form >
           <label>
-            Name:
-            <input type="text" name="name" />
+            User Name:
+            <input type="text" name="name" required />
           </label>
-            <input type="submit" value="Submit" />
+          
+          <label>
+            Password:
+            <input type="password" required />
+          </label>
+
+          <button onClick={handleSubmit}>Submit</button>
+
       </form>
     </div>
   )
